3dd7f3545f174f4ece661c9eeee1ae845ccd2d79
Marcus Jensen <marcusjensen@Marcuss-MacBook-Pro.local>
2020-10-24 12:44:57 -0700

    query using regular expressions

--- index.js
+++ index.js
@@ -56,10 +56,39 @@ async function getCourses() {
 
 
     const courses = await Course
-        .find({ author: 'marcus', isPublished: true})
+        // .find({ author: 'marcus', isPublished: true})
+
+        //in order to query the concept of greater than 10, we must user one of these operators as a key. These operators are decorated with a '$' when used. 
+        //also needs to be passed within an object.
+        //we can pass multiple comparison operators. (below is greater than 10 dollars and less than 20) 
+        // .find({ price: {$gt: 10, $lte: 20 }})
+        //below is a query returning courses that are either: 10, 15 or 20 dollars
+        // .find({price: { $in: [10,15,20]}})
+
+
+        //The logical operators in Node are 'or' and 'and'. below is them being used
+        //in the or statement below, we are gettin courses where author is marcus and where isPublished equals true. The 'and' operator is used in the same way.
+
+
+        // .find()
+        // .or([{author: 'marcus'}, {isPublished: true}])
+        // .and([])
+
+        //below is how regex works in node. below is how we would write a query: starts with marcus. ^ is 'starts with'
+
+        // .find({author: /^marcus/})
+
+        //below is 'ends with'. annotated by '$'. 'i' means case insensitive.
+        // .find({author: /jensen$/i})
+
+        //below is 'contains' marcus. .*stuff.* means we can have 0 or more characters before or after. the i at the end makes it case insensitive. 
+
+.find({ author: /.*mosh.*/i})
         .limit(10)
         .sort({ name: 1})
         .select({ name: 1, tags: 1});
+
+        
 
     console.log(courses);
 
