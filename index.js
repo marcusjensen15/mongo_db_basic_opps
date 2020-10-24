0913a2191d2451ee97107f0e904d8c91ef085571
Marcus Jensen <marcusjensen@Marcuss-MacBook-Pro.local>
2020-10-24 11:30:44 -0700

    sucessfully connecting to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
 //below line changes when we are deploying to a production string
-mongoose.connect('mongodb://localhost');
+//this connect method returns a promise, so we can chain using 'then'
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('connected to mongodb'))
+    .catch(err => console.log("could not connect to mongo db", err));
 
